792f056b561dbd6c743ff63687d1eaf9ff0a86cc
Anil Attada <anilattada222@gmail.com>
2026-03-18 16:15:24 +0530

    EXPENSE added

--- expense.sh
+++ expense.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AMI_ID="ami-02dfbd4ff395f2a1b" #temporary
+AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-06f23152b42979b41"
 INSTANCES=( "mysql" "backend" "frontend" )
 ZONE_ID="Z1033319ZTEWWV8J5PPG"
@@ -8,7 +8,7 @@ DOMAIN_NAME="anilkumar.shop"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-02dfbd4ff395f2a1b --instance-type t3.micro --security-group-ids sg-06f23152b42979b41 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-06f23152b42979b41 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
